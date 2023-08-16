bdb4b05bb12debda91885f4de27e081adfa29eef
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-16 21:24:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -28,17 +28,17 @@ def process(image):
     
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    """
+
     mp_drawing.draw_landmarks(
         image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
     mp_drawing.draw_landmarks(
         image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(
         image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    #mp_drawing.draw_landmarks(
+    #    image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    
     """
-
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -47,7 +47,8 @@ def process(image):
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-          
+    """
+    
     return cv2.flip(image, 1)
 
 RTC_CONFIGURATION = RTCConfiguration(
