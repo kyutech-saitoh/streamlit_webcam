07c613f27207369db416db5163b51b14a03e117a
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-16 21:15:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import mediapipe as mp
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 mp_drawing = mp.solutions.drawing_utils
-#mp_drawing_styles = mp.solutions.drawing_styles
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
 
 holistic = mp_holistic.Holistic(
@@ -13,13 +13,22 @@ holistic = mp_holistic.Holistic(
     min_tracking_confidence=0.5
 )
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    model_complexity=0,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5
+)
+
 def process(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
-    results = holistic.process(image)
-
+    #results = holistic.process(image)
+    results = hands.process(image)
+    
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    """
     mp_drawing.draw_landmarks(
         image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
     mp_drawing.draw_landmarks(
@@ -28,7 +37,17 @@ def process(image):
         image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(
         image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-    
+    """
+
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+          
     return cv2.flip(image, 1)
 
 RTC_CONFIGURATION = RTCConfiguration(
