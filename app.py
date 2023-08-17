3c539ff019d6153fe717772bec9bfdbf88d57b9c
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 15:08:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,25 +4,8 @@ import av
 import mediapipe as mp
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
-mp_face_mesh = mp.solutions.face_mesh
-face_mesh = mp_face_mesh.FaceMesh(
-    static_image_mode=True,
-    refine_landmarks=True,
-    max_num_faces=1,
-    min_detection_confidence=0.5
-)
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-"""
-mp_holistic = mp.solutions.holistic
-
-holistic = mp_holistic.Holistic(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
-"""
-
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     model_complexity=0,
@@ -34,42 +17,10 @@ def process(image):
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
-    """
-    results = holistic.process(image)
-    results = face_mesh.process(image)
-    """
-    
+
+    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    st.write("check")
-    print("check")
-    """
-    for face in results.multi_face_landmarks:
-       for i, landmark in enumerate(face.landmark):
-            # 特徴点の座標の取得
-            x = landmark.x
-            y = landmark.y
-            z = landmark.z
-
-            points.append((x, y, z))
-            print ("No.%d, (%f, %f, %f)" % (i, x, y, z))
-            
-            x = int(x * image_width)
-            y = int(y * image_height)
-            cv2.circle(image, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
-            cv2.circle(image, center=(x, y), radius=2, color=(255, 255, 255), thickness=-1)
-           
-    mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
-    """
-    
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         mp_drawing.draw_landmarks(
@@ -78,8 +29,8 @@ def process(image):
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
-    
     return cv2.flip(image, 1)
+
 
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -87,8 +38,6 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 class VideoProcessor:
     def recv(self, frame):
-        st.write("check0")
-        print("check0")
         img = frame.to_ndarray(format="bgr24")
 
         img = process(img)
