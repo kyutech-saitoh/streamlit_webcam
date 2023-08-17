85367880a2755696a199e58a3c07831f143cbd3b
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 18:18:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def process(image, is_image, is_landmarks):
         black_image = np.zeros((image_height, image_width, 3), np.uint8)
         white_image = black_image + 255
 
-        if is_blank == True:
+        if is_image == True:
             out_image = black_image.copy()
             
         if is_landmarks == True:
