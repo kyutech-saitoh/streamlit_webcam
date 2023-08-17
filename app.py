338058ad13036181f0dce5aa691ecdf0342d14f2
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 16:29:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,10 @@ from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 
 def process(image):
-    drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
+    #drawing_spec = mp.solutions.drawing_utils.DrawingSpec(thickness=2, circle_radius=1)
 
-    """
+    out_image = image.copy()
+
     with mp.solutions.face_mesh.FaceMesh(
         static_image_mode=True,
         max_num_faces=5,
@@ -20,20 +21,15 @@ def process(image):
     ) as face_mesh:
 
         results = face_mesh.process(image)
-        out_image = image.copy()
-
-        st.write("detected")
 
         for face_landmarks in results.multi_face_landmarks:
             mp.solutions.drawing_utils.draw_landmarks(
                 image=out_image,
                 landmark_list=face_landmarks,
-                connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,
-                landmark_drawing_spec=drawing_spec
+                connections=mp.solutions.face_mesh.FACEMESH_CONTOURS
             )
-    """
     
-    return cv2.flip(image, 1)
+    return cv2.flip(out_image, 1)
     
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
