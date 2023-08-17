199758488bc1420e255c66c943f20e012a324aae
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 18:06:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,11 +18,11 @@ def process(image, is_image, is_landmarks):
 
         (image_height, image_width) = image.shape[:2]
 
-#        blank = np.zeros((image_height, image_width, 3))
-#        blank += 255
+        blank = np.zeros((image_height, image_width, 3))
+        blank += 255
 
 #        if is_blank == True:
-#        out_image = blank.copy()
+        out_image = blank.copy()
             
 #        if is_landmarks == True:
         if results.multi_face_landmarks:
