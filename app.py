4538a4e438d4698b70d3eebfc604b93cde4dd328
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 16:37:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,32 @@ def process(image):
     ) as face_mesh:
         
         results = face_mesh.process(image)
-    
+
+        """
         for face_landmarks in results.multi_face_landmarks:
             mp.solutions.drawing_utils.draw_landmarks(
                 image=out_image,
                 landmark_list=face_landmarks,
                 connections=mp.solutions.face_mesh.FACEMESH_CONTOURS
             )
+        """
+        (image_height, image_width) = image.shape[:2]
+        
+        for face in results.multi_face_landmarks:
+           for i, landmark in enumerate(face.landmark):
+                # 特徴点の座標の取得
+                x = landmark.x
+                y = landmark.y
+                z = landmark.z
     
+                points.append((x, y, z))
+                print ("No.%d, (%f, %f, %f)" % (i, x, y, z))
+                
+                x = int(x * image_width)
+                y = int(y * image_height)
+                cv2.circle(out_image, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
+                cv2.circle(out_image, center=(x, y), radius=2, color=(255, 255, 255), thickness=-1)    
+
     return cv2.flip(out_image, 1)
     
 RTC_CONFIGURATION = RTCConfiguration(
