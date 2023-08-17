2f3672df0c154fcd45371b86887e59810fd3957e
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-17 13:40:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import av
 import mediapipe as mp
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
+mp_face_mesh = mp.solutions.face_mesh
+
+"""
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_holistic = mp.solutions.holistic
@@ -19,22 +22,47 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
+"""
 
 def process(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
     #results = holistic.process(image)
-    results = hands.process(image)
+    #results = hands.process(image)
     
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-    mp_drawing.draw_landmarks(
-        image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
-    mp_drawing.draw_landmarks(
-        image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    with mp_face_mesh.FaceMesh(
+        static_image_mode=True,
+        refine_landmarks=True,
+        max_num_faces=1,
+        min_detection_confidence=0.5) as face_mesh:
+    
+        # Convert the BGR image to RGB and process it with MediaPipe Face Mesh.
+        results = face_mesh.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    
+        for face in results.multi_face_landmarks:
+           for i, landmark in enumerate(face.landmark):
+                # 特徴点の座標の取得
+                x = landmark.x
+                y = landmark.y
+                z = landmark.z
+    
+                points.append((x, y, z))
+                #print ("No.%d, (%f, %f, %f)" % (i, x, y, z))
+                
+                x = int(x * image_width)
+                y = int(y * image_height)
+                cv2.circle(image, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
+                cv2.circle(image, center=(x, y), radius=2, color=(255, 255, 255), thickness=-1)
+           
+    #mp_drawing.draw_landmarks(
+    #    image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS)
+    #mp_drawing.draw_landmarks(
+    #    image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+    #mp_drawing.draw_landmarks(
+    #    image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     #mp_drawing.draw_landmarks(
     #    image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
     
