46b005ba9a6239244296637995612de4402b1309
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-18 00:18:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def process(image, is_show_image, draw_pattern):
     with mp.solutions.face_mesh.FaceMesh(
         static_image_mode=True,
         max_num_faces=1,
+        refine_landmarks=True,
         min_detection_confidence=0.5
     ) as face_mesh:
 
