4240912a746c0f52b0c0c9d6c51cef8078518cd7
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-18 00:12:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -80,10 +80,10 @@ def process(image, is_show_image, draw_pattern):
                         x = int(x * image_width)
                         y = int(y * image_height)
     
-                        if idx == 468:
-                            cv2.circle(out_image, center=(x, y), radius=5, color=(200, 200, 255), thickness=-1)
-                        elif idx == 473:
-                            cv2.circle(out_image, center=(x, y), radius=5, color=(255, 200, 200), thickness=-1)    
+                        if idx in left_iris_idxs:
+                            cv2.circle(out_image, center=(x, y), radius=2, color=(0, 0, 255), thickness=-1)
+                        if idx in rightt_iris_idxs:
+                            cv2.circle(out_image, center=(x, y), radius=2, color=(0, 255, 0), thickness=-1)    
     
     return cv2.flip(out_image, 1)
     
