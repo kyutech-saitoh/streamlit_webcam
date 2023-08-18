abb32ff9f503dd11273db644725d994cdd677f0c
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-18 11:48:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,16 +18,15 @@ def drawB(image, face, image_width, image_height):
     contours = []
     contour = []
     for idx in left_eye_idxs:
-#    for idx in range(100):
         x = func(face.landmark[idx].x, image_width)
         y = func(face.landmark[idx].y, image_height)
 
         cv2.circle(image, (x, y), 2, color=(150, 150, 0), thickness=-1)
-#        contour.append((x, y))
+        contour.append((x, y))
 
 #    contours.append(contour)
 
-#    cv2.drawContours(image, contours, -1, (0, 0, 255), thickness=2)
+    cv2.drawContours(image, contour, -1, (0, 0, 255), thickness=2)
     
     return image
     
