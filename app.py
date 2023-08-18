61979546c8823a09fd7bc7cc3d6493f3ba9a1d24
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-18 11:36:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def func(value1, value2):
 
 
 def drawB(image, face, image_width, image_height):
-    left_eye_idxs = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155]
+    left_eye_idxs = {133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155}
 
     contours = []
     contour = []
